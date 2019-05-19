7bebd8ef76351095be729040c122590b715e458b
heshamghalaab <hesham_ghalaab@hotmail.com>
2019-05-20 00:37:47 +0200

    Initial Commit

---
+++ NotificationBanner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NotificationBanner
+//
+//  Created by hesham ghalaab on 5/20/19.
+//  Copyright © 2019 hesham ghalaab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
